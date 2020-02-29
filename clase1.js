8b285325ea6146cf983bec05b47f0db846cdefe3
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-29 00:15:04 -0500

    variables

---
+++ clase1.js
@@ -0,0 +1,11 @@
+
+// NOTE  declaracion con var ; es opcional
+var nombre = 'Nicolas', apellido = 'Castañeda' //en JS se usa ' 
+var edad = 23;
+ 
+edad = '28 años' // se sobre escribe la variable como si fueran dinamicas
+
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo '+ edad + ' años')
+//NOTE JS es debilmente tipado se debe tener cuidado de sobre escribir estas
