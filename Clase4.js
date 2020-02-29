c3c4e6493e81d0a2a4e213d57ef76ebb3988e1c2
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-29 14:59:12 -0500

    funciones

---
+++ Clase4.js
@@ -0,0 +1,8 @@
+var nombre = 'sacha', edad = 28
+
+function imprimirEdad(nombre, edad) {
+    console.log(`${nombre} tiene ${edad} años`)
+}
+
+imprimirEdad('Nicolas',23)
+// NOTE palabra this indica que se usa la variable de la clase y no la de la funcion
