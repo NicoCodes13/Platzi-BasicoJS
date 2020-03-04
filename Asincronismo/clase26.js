d6c08886e8f7f7ff51af85091dbd7b1a0cf874e2
Nicolas Castañeda <nickcar13@gmail.com>
2020-03-04 15:22:33 -0500

    Tiempo en JS

--- Asincronismo/clase26.js
+++ Asincronismo/clase26.js
@@ -1,55 +1,7 @@
-class Persona {
-    constructor(nombre, apellido, altura) {
-        this.nombre = nombre,
-            this.apellido = apellido,
-            this.altura = altura
-    }
+// console.log('a')
+// //NOTE indica que espere la cantidad de tiemoo en ms para ejecutar la funcion
+// //NOTE El setTimeout se ejecuta en la cola de tareas al terminar de ejecutar sus tareas principales osea hasta que termine su proceso
+// setTimeout(() => console.log('b'), 0) 
+// console.log('c')
 
-    saludar(fn) {
-        var {nombre, apellido} = this
-        console.log(`Hola, me llamo ${nombre} ${apellido}`)
-        if (fn) {
-            fn(nombre, apellido)
-        }
-    }
-
-    soyAlto() {
-        return this.altura > 1.8
-    }
-
-    imprimirSoyAlto() {
-        if (this.soyAlto()) {
-            return console.log(`${this.nombre} es alto porque mide ${this.altura}m`)
-        } else {
-            return console.log(`${this.nombre} no es alta porque mide ${this.altura}m`)
-        }
-    }
-}
-
-class Desarrollador extends Persona {
-    constructor(nombre, apellido, altura) {
-        super(nombre, apellido, altura) // llama el constructor de la super clase
-    }
-    saludar(fn) {
-        var {nombre, apellido} = this
-        console.log(`Hola me llamo ${nombre} ${apellido} y soy desarrollador`)
-        if (fn) {
-            fn(nombre, apellido, true)
-        }
-    }
-}
-
-function responderSaludo(nombre, apellido, esDev) {
-    console.log(`Buen dia ${nombre} ${apellido}`)
-    if (esDev) {
-        console.log(`ah mira no sabia que eras dev`)
-    }
-}
-
-var sacha = new Persona('Sacha', 'Lifszync', 1.72)
-var erika = new Persona('Erika', 'Luna', 1.65)
-var arturo = new Desarrollador('Arturo', 'Martinez', 1.89)
-
-sacha.saludar()
-erika.saludar(responderSaludo) //al pasar la funcion se manda sin () y se debe evaluar si se usa o no en la funcion y posteriormente llamada
-arturo.saludar(responderSaludo)
+setTimeout(()=> console.log('d'),2000)
